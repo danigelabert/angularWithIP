12b6772fe7a4f1fdcfaa6383a032e031991567f8
aramm <aram.mateos@institutvidreres.cat>
2022-12-12 09:13:05 +0100

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PaginaWebComponent } from './pagina-web/pagina-web.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaWebComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pagina-web/pagina-web.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-web',
+  templateUrl: './pagina-web.component.html',
+  styleUrls: ['./pagina-web.component.css']
+})
+export class PaginaWebComponent {
+
+}
