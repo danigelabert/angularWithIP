815a43b1a0da6c8f46475cd8a9ce17218d39a257
danigelabert <73782173+danigelabert@users.noreply.github.com>
2022-12-14 08:44:28 +0100

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PaginaWebComponent } from './pagina-web/pagina-web.component';
+import { MovilesComponent } from './moviles/moviles.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { Formulario2Component } from './formulario2/formulario2.component';
+import { OrdenadoresComponent } from './ordenadores/ordenadores.component';
+import { PerifericosComponent } from './perifericos/perifericos.component';
+import { CondicionesComponent } from './condiciones/condiciones.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaginaWebComponent
+    PaginaWebComponent,
+    MovilesComponent,
+    CarritoComponent,
+    ContactoComponent,
+    FormularioComponent,
+    Formulario2Component,
+    OrdenadoresComponent,
+    PerifericosComponent,
+    CondicionesComponent
   ],
   imports: [
     BrowserModule
