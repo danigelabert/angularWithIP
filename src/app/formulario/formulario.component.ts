dbbc92864137d694ada0c03abfccd86ddf20335b
aramm <aram.mateos@institutvidreres.cat>
2022-12-14 09:27:51 +0100

    Arreglar Errors

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent {
+export class FormularioComponent implements OnInit{
+  email: any;
+  password:any;
 
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+
+  valorLogIn(){
+    console.log(this.email+" "+this.password)
+  }
 }
