4e45057abdcc21761f44cc5d1a622119749efd6d
danigelabert <73782173+danigelabert@users.noreply.github.com>
2023-02-27 09:52:02 +0100

    INICI FET

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {ServeiService} from "../servei.service";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 
 @Component({
   selector: 'app-formulario',
@@ -9,25 +9,34 @@ import {HttpClient} from "@angular/common/http";
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit{
-  usuari: any;
-  password:any;
+  correu: any;
+  nombre:any;
 
   constructor(private router: Router, private http: HttpClient) {
-    var usr=http.get<any>("http://localhost:4080/registre").subscribe((doc)=>{
-      console.log(doc)
-    })
+
   }
   ngOnInit() {
   }
   formularioEnviado(){
+    var resultat: Object =false;
+    let req = new HttpParams().set('email',this.correu);
+    let req2 = new HttpParams().set('name',this.nombre);
+    this.http.get("http://localhost:4080/inicisessio", {params: req}).subscribe((client)=>{
+      resultat=client;
+      console.log(resultat);
+      if(resultat==true){
+        this.http.get("http://localhost:4080/contrasenya", {params: req2}).subscribe((client)=> {
+          resultat = client;
+          console.log(resultat);
+          if (resultat == true) {
+            alert("TOT CORRECTE")
+          }else
+            alert("Contrasenya incorrecte.")
+        })
 
-    // if (usr==this.usuari && pasw==this.password){
-    //   const nav: string[] = ['/pagina-web']
-    //   if($myParam.length) {
-    //     nav.push($myParam);
-    //   }
-    //   this.router.navigate(nav)
-    //   localStorage.setItem("nombre",usr!)
-    // }
+      }else{
+        alert("Este correo no existe, introduce de nuevo tu correo")
+      }
+    })
   }
 }
